c4469a7bb8cf981cb4e228aeaebf064da49cd69a
Christian Reubrez <christian.reubrez@gmail.com>
2023-03-03 11:21:43 +0100

    new commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/year')
+fetch('whatyearisit-backend-beta-red.vercel.app/year')
     .then(response => response.json())
     .then(data => {
         // console.log(data);
