6ddc2ed3eac7d8f014a423af01ff88afdb54361f
Christian Reubrez <christian.reubrez@gmail.com>
2023-03-03 11:27:27 +0100

    third commit

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-fetch('whatyearisit-backend-beta-red.vercel.app/year')
+fetch('https://whatyearisit-backend-beta-red.vercel.app/year')
     .then(response => response.json())
     .then(data => {
-        // console.log(data);
-
-        document.querySelector('#year').textContent +=
-            `${data.year}`
-            ;
+   
+        document.querySelector('#year').textContent = data.year;
     });
     
