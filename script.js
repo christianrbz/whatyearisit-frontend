c5040b9819dd7fa5a778d3ac8973cea306535189
Christian Reubrez <christian.reubrez@gmail.com>
2023-03-03 11:00:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+fetch('http://localhost:3000/year')
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data);
+
+        document.querySelector('#year').textContent +=
+            `${data.year}`
+            ;
+    });
+    
